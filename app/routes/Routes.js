28dbcd42c502caed8014742a46df548b64eec02c
= <houyaowei@gmail.com>
2018-04-22 19:29:01 +0800

    route update

--- app/routes/Routes.js
+++ app/routes/Routes.js
@@ -2,29 +2,27 @@ import React from "react";
 import {
     Router,
     Route,
+    Redirect,
     Switch
 } from 'react-router-dom';
 import history from './history';
-import Container from "../container/Container"
-import AsideContainer from "../container/AsideContainer"
-import RenderRoutes from '../components/commons/RenderRoutes';
-
 import LoginContainer from "../components/login/LoginContainer";
-//import Login from "../components/login/Login";
+import Container from "../container/Container";
+import RenderRoutes from './RenderRoutes';
+
 import  Conf from "../components/config/layout" 
 import Home from "../components/home/Home";
 import table from "../components/home/aside/Table";
 import Card from "../components/home/aside/Card";
 import Chart from "../components/home/aside/Chart";
 import Analysis from "../components/analysis/Analysis";
-import warn from "../components/warn/warnLayout"
+import warn from "../components/warn/warnLayout";
+
 //global router config
-const routesConfig = [
-    { path: '/home',
-      component: Home
-    },
-    { path: '/config',
-    component: Conf
+export const routesConfig = [
+    { 
+        path: '/config',
+        component: Conf
     },
     {
       path:'/warning',
@@ -32,7 +30,6 @@ const routesConfig = [
     },
     {
       path: "/basic",
-      component: AsideContainer,
       children:[
         {
           path : '/basic/table',
@@ -53,19 +50,21 @@ const routesConfig = [
         component: Analysis
     }
 ]
-
 export default () => (
-    <Router history={history}>
-      <Switch>
-        <Route path='/' exact component={LoginContainer} />
-        <Route path='/login' exact component={LoginContainer} />
-        {/* <Container>
-          <Switch>
-            {routesConfig.map((route, i) => (
-              <RenderRoutes key={i} {...route} />
-            ))}
-          </Switch>
-        </Container> */}
-      </Switch>
-    </Router>
+  <Router history={history}>
+  <Switch>
+    <Route path="/" exact component={LoginContainer}>
+      <Redirect to="/login" />
+    </Route>
+    <Route path='/login' exact component={LoginContainer} />
+       <Container>
+        <Switch>
+          {routesConfig.map((route, i) => (
+            <RenderRoutes key={i} {...route} />
+          ))}
+        </Switch>
+        </Container>
+  
+  </Switch>
+</Router>
   )
