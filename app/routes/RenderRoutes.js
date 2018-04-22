cfb28447afadec7c5b8ee739f9862d545e361c27
= <houyaowei@gmail.com>
2018-04-22 19:30:32 +0800

    route update

---
+++ app/routes/RenderRoutes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+export default route => (
+  <Route path={route.path} render={props => (
+      <route.component {...props} routes={route.children} path={route.path} />
+    )
+  }/>
+)
