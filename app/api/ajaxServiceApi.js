4d37f8059ab9b659187d79fa76519ba9d0333eb2
houyaowei <houyaowei@gmail.com>
2018-11-17 21:35:29 +0800

    1、delete package-lock.json  2、重构代码

--- app/api/ajaxServiceApi.js
+++ app/api/ajaxServiceApi.js
@@ -1,13 +1,29 @@
-class TravelServiceApi {
-    static login(){
-        console.log("TravelServiceApi -> login");
-        return new Promise((resolve) => {
-            //setTimeout(() => {
-              resolve(Object.assign({}, {
-                   login : true
-              }));
-           // }, 3000);
-        });
-    }
+import fetch from "whatwg-fetch";
+import URL from "./urlConfig";
+
+let getData = function(url, data, resolve) {
+  let option = {
+    method: "POST",
+    credentials: "include",
+    headers: { "Content-type": "application/json; charset=utf-8" },
+    body: data && JSON.stringify(data)
+  };
+  return fetch(url, option)
+    .then(resp => resp.json())
+    .then(data => {
+      resolve({
+        res: data
+      });
+    });
+};
+class AjaxServiceApi {
+  static login() {
+    console.log("AjaxServiceApi -> login");
+    return new Promise(resolve => {
+      let url = URL.login;
+      let postData = {};
+      getData(url, postData, resolve);
+    });
+  }
 }
-export default TravelServiceApi;
+export default AjaxServiceApi;
